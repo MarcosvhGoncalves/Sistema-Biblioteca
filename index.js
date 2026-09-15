1b1207c42a0b4297591393cee928d50e575fd070
Marcos Vinnicius <marcosvhgoncalves@gmail.com>
2026-09-15 17:39:08 -0300

    Feat: Adição de tratamentos de erros

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import * as readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
 import { LivroFisico, Ebook } from "./TiposDeItens.js";
 import { Leitor } from "./Leitor.js";
+import { error } from "node:console";
 const rl = readline.createInterface({ input, output });
 
 (async function iniciarSistema() {
@@ -13,17 +14,28 @@ const rl = readline.createInterface({ input, output });
     await rl.question("Digite a idade do leitor: "),
   );
   const leitor = new Leitor(nomeLeitor, idadeInformada);
+  if (!nomeLeitor || !idadeInformada){
+    throw new error("======================== Nome ou idade do leitor não informadas ========================");
+  
+  }
   console.log("\nSelecione o tipo de livro: ");
   console.log("1- Livro Físico");
   console.log("2- Ebook");
   const tipoLivro = await rl.question("Digite a opção: ");
 
-  if (tipoLivro === "1" || tipoLivro === "2") {
+  if (tipoLivro > 2){
+    throw new error("======================== Opção inválida! ========================");
+  }
+
     const titulo = await rl.question("Digite o título: ");
     const autor = await rl.question("Digite o autor: ");
     const anoInfo = Number(await rl.question("Digite o ano de publicação: "));
 
-    switch (tipoLivro) {
+    if (!titulo || !autor){
+      throw new error("======================== Nome do autor ou titulo não informados ========================");
+    }
+
+     switch (tipoLivro) {
       case "1":
         const corredor = await rl.question("Digite o corredor: ");
         livro = new LivroFisico(titulo, autor, anoInfo, corredor);
@@ -34,18 +46,15 @@ const rl = readline.createInterface({ input, output });
         );
         livro = new Ebook(titulo, autor, anoInfo, formatoArquivo);
     }
-  } 
-  else {
-    throw new Error("Opção inválida!");
-  }
-
+  
   const diasAtraso = parseInt(await rl.question("Dias de atraso: "));
+
+  if (!diasAtraso){
+    console.log("Informe os dias de atraso, caso não haja atraso digite '0'.")
+  }
 
   const valorMulta = livro.calcularMulta(diasAtraso);
 
-  if (livro.titulo === undefined || livro.autor === undefined) {
-    console.log("\n [ERRO]: Titulo de livro ou autor não classificados.");
-  } else {
     console.log("\n============================================");
     console.log("========== Etiqueta de Emprestimo ==========");
     console.log("============================================");
@@ -53,7 +62,6 @@ const rl = readline.createInterface({ input, output });
     console.log(`Autor do livro: ${livro.autor}`);
     console.log(`Ano do livro: ${livro.anoPublicacao}`);
     console.log(`\n Valor da multa por atraso:: R$ ${valorMulta.toFixed(2)}`);
-  }
 
   rl.close();
 })();
